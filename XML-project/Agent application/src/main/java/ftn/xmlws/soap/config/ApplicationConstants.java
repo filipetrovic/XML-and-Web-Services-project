c4437040bc72c0c49d7e78c4758b3cd92fcf8535
sansajn5n <mudrinic5n@outlook.com>
2018-06-14 01:40:42 +0200

    feature/soap-setup

--- XML-project/Agent application/src/main/java/ftn/xmlws/soap/config/ApplicationConstants.java
+++ XML-project/Agent application/src/main/java/ftn/xmlws/soap/config/ApplicationConstants.java
@@ -6,4 +6,11 @@ package ftn.xmlws.soap.config;
  */
 public interface ApplicationConstants {
 
+    public static String ENDPOINT_AGENT = "http://ftn.xmlws.com/soap";
+    public static String LOCAL_PART_AGENT = "getAgentRequest";
+
+    public static String CONFIG_LOCATION_URI = "/soapWS";
+    public static String CONFIG_SERVICE = "AgentServicePort";
+    public static String CONFIG_SERVLET_REGISTRATION = "/soapWS/*";
+    public static String CONFIG_SCHEMA_LOCATION = "schemas/agent.xsd";
 }
---
+++ XML-project/Agent application/src/main/java/ftn/xmlws/soap/config/SoapWebConfig.java
@@ -0,0 +1,43 @@
+package ftn.xmlws.soap.config;
+
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class SoapWebConfig extends WsConfigurerAdapter {
+
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, ApplicationConstants.CONFIG_SERVLET_REGISTRATION);
+    }
+
+    @Bean
+    public XsdSchema agentSchema() {
+        return new SimpleXsdSchema(new ClassPathResource(ApplicationConstants.CONFIG_SCHEMA_LOCATION));
+    }
+
+    @Bean
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema agentSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setSchema(agentSchema);
+        definition.setLocationUri(ApplicationConstants.CONFIG_LOCATION_URI);
+        definition.setPortTypeName(ApplicationConstants.CONFIG_SERVICE);
+        definition.setTargetNamespace(ApplicationConstants.ENDPOINT_AGENT);
+        return definition;
+    }
+
+}
