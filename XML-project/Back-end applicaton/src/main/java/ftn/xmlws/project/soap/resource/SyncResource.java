dce86ad2825a22688d540c74f62c3bce92fe55c5
Filip Petrovic <fpetrovic@ymail.com>
2018-06-16 21:09:55 +0200

    Mudri sjebo projekat

--- XML-project/Back-end applicaton/src/main/java/ftn/xmlws/project/soap/resource/SyncResource.java
+++ XML-project/Back-end applicaton/src/main/java/ftn/xmlws/project/soap/resource/SyncResource.java
@@ -1,14 +1,9 @@
 package ftn.xmlws.project.soap.resource;
 
-import ftn.xmlws.project.config.ApplicationConstants;
-import com.xmlws.ftn.soap.GetSyncResponse;
-import com.xmlws.ftn.soap.GetSyncRequest;
-import ftn.xmlws.project.soap.service.SyncService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import ftn.xmlws.project.soap.service.SyncService;
 
 /**
  * Endpoint for syncing resource from core app
@@ -19,15 +14,15 @@ public class SyncResource {
 
     @Autowired
     private SyncService syncService;
-
-	@PayloadRoot(namespace = ApplicationConstants.ENDPOINT_AGENT, localPart = ApplicationConstants.LOCAL_PART_AGENT)
-    @ResponsePayload
-    public GetSyncResponse getSyncResponse(@RequestPayload GetSyncRequest request) {
-        GetSyncResponse response = new GetSyncResponse();
-        syncService.getTypesAsXML().stream().forEach( dao -> response.getEncodedAccommodationType().add(dao));
-        syncService.getFacilitysAsXML().stream().forEach( dao -> response.getEncodedFacility().add(dao));
-        syncService.getStarRatingAsXML().stream().forEach( dao -> response.getEncodedStarRating().add(dao));
-        return response;
-    }
+//
+//	@PayloadRoot(namespace = ApplicationConstants.ENDPOINT_AGENT, localPart = ApplicationConstants.LOCAL_PART_AGENT)
+//    @ResponsePayload
+//    public GetSyncResponse getSyncResponse(@RequestPayload GetSyncRequest request) {
+//        GetSyncResponse response = new GetSyncResponse();
+//        syncService.getTypesAsXML().stream().forEach( dao -> response.getEncodedAccommodationType().add(dao));
+//        syncService.getFacilitysAsXML().stream().forEach( dao -> response.getEncodedFacility().add(dao));
+//        syncService.getStarRatingAsXML().stream().forEach( dao -> response.getEncodedStarRating().add(dao));
+//        return response;
+//    }
 
 }
